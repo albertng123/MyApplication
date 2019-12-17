79c2bfda61fe2340b4d547dbf1b46defb7cebeef
albertng123 <albertbobby94@gmail.com>
2019-12-17 23:31:56 +0700

    New

---
+++ app/src/main/java/com/example/myapplication/CreateAccount.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+
+    public void goToSignIn(View view){
+        Intent intent = new Intent(CreateAccount.this, LoginPage.class);
+        startActivity(intent);
+        finish();
+    }
+}
--- app/src/main/java/com/example/myapplication/LoginPage.java
+++ app/src/main/java/com/example/myapplication/LoginPage.java
@@ -2,14 +2,31 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class LoginPage extends AppCompatActivity {
 
+    TextView createAccount,forgotPass;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_page);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        createAccount = findViewById(R.id.createAccount);
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginPage.this, CreateAccount.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
